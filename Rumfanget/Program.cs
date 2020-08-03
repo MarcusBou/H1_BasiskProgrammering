d5f35fa9352d3e9af9db50b9707e9c9436c444b5
Marcus Bournonwille <Marc8847@zbc.dk>
2020-08-03 12:22:18 +0200

    Rumfang

---
+++ Rumfanget/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Rumfanget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double h, b, l;
+
+            Console.WriteLine("input kassens højde længde og bredde, nedenunder.");
+            Console.Write("Højde: ");
+            h = Double.Parse(Console.ReadLine());
+            Console.Write("Bredde: ");
+            b = Double.Parse(Console.ReadLine());
+            Console.Write("længde: ");
+            l = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Volumen på kassen med målene " + h +" i højde " + b + " i bredde" + l + " i længde" +
+                "\nHar Volumen/rumfanget: " + Volume(h,b,l));
+
+            Console.ReadKey();
+        }
+
+        static double Volume(double H, Double B, Double L) {
+            double output = H * B * L;
+            return output;
+        }
+    }
+}
