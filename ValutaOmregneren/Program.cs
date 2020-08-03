72ddc59e429334c659c88e72dc7e8c8c37cc029a
Marcus Bournonwille <Marc8847@zbc.dk>
2020-08-03 12:01:12 +0200

    ValutaOmregner

---
+++ ValutaOmregneren/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+
+namespace ValutaOmregneren
+{
+    class Program
+    {
+        static double dkk, usd, pund, euro, sek;
+
+        static void Main(string[] args)
+        {
+                Console.Write("Værdien du vil have omregnet i DKK: ");
+                dkk = double.Parse(Console.ReadLine());
+
+                convert(dkk);
+
+                Console.Clear();
+
+                Console.WriteLine(dkk + "kr. i de forskellige valutaer\n\n" +
+                    "US dollar: " + usd + "\n" +
+                    "Britiske Pund: " + pund + "\n" +
+                    "Euro: " + euro + "\n" +
+                    "Svensk Krone: " + sek);
+
+            Console.ReadKey();
+        }
+
+        static void convert(double Dkk) 
+        {
+            usd = Dkk * 6.32;
+            pund = Dkk * 8.28;
+            euro = Dkk * 7.44;
+            sek = Dkk * 0.72;
+        }
+    }
+}
