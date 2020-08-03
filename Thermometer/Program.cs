45b55ff14d34e3d70446c3018d78c5f0e96258fd
Marcus Bournonwille <Marc8847@zbc.dk>
2020-08-03 11:01:04 +0200

    Termometer

---
+++ Thermometer/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace Thermometer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius, fahrenheit, reamur;
+
+            Console.Write("Din celsius grad: ");
+            celsius = Double.Parse(Console.ReadLine());
+
+            reamur = celsius * 0.8;
+            fahrenheit = celsius * 1.8 + 32;
+
+            Console.WriteLine("Grader i Reamur: " + reamur + "\nGrader i fahrenheit: " + fahrenheit);
+            Console.ReadKey();
+        }
+    }
+}
