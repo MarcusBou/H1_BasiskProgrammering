0122ea53cb971b0962e107db313a54a20054f1c1
Marcus Bournonwille <Marc8847@zbc.dk>
2020-08-03 10:02:30 +0200

    Add project files.

---
+++ H1_BasiskProgrammering/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace H1_BasiskProgrammering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
