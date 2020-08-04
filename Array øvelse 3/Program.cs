1d77d8916bcf15530682405d4a424f7d62eccb24
Marcus Bournonwille <Marc8847@zbc.dk>
2020-08-04 13:23:25 +0200

    Post

---
+++ Array øvelse 3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+
+namespace Array_øvelse_3
+{
+    class Program
+    {
+        static ArrayList boyName = { "William", "Oliver", "Noah", "Emil", "Victor", "Magnus", "Frederik", "Mikkel", "Lucas", "Alexander", "Oscar", "Mathias",
+                                    "Sebastian", "Malthe", "Elias", "Christian", "Mads", "Gustav", "Villads", "Tobias" };
+    static string searchWord;
+        static void Main(string[] args)
+        {
+            boyName
+            {
+                Array.Sort(boyName);
+                for (int i = 0; i < boyName.Length; i++)
+                {
+                    Console.WriteLine(boyName[i]);
+                }
+                Console.Write("\nSearch: ");
+                searchWord = Console.ReadLine();
+                for (int i = 0; i < boyName.Length - 1; i++)
+                {
+                    if (boyName[i].Contains(searchWord))
+                        Console.WriteLine(boyName[i].ToString());
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
