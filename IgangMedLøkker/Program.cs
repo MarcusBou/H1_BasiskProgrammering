6821a9d6428702d9190225ec03303e1b1327efb0
Marcus Bournonwille <Marc8847@zbc.dk>
2020-08-04 08:28:42 +0200

    Igang med løkker

---
+++ IgangMedLøkker/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace IgangMedLøkker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A: for loop runs up to a hundred
+            for (int i = 0; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            //B: for Loop runs to hundred but only print to fifty
+            for (int j = 0; j < 100; j++)
+            {
+                if (j <= 50)
+                {
+                    Console.WriteLine(j);
+                }
+            }
+
+            //C: While loop runs to hundred
+            int k = 0;
+            while (k <= 100)
+            {
+                Console.WriteLine(k);
+                k++;
+            }
+
+            //for loop runs to zero from hundred
+            for (int l = 100; l > 0; l--)
+            {
+                Console.WriteLine(l);
+            }
+        }
+    }
+}
