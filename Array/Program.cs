eaf1347496ee3452329a255ae2d2ab66a9944c32
Marcus Bournonwille <Marc8847@zbc.dk>
2020-08-04 09:19:08 +0200

    Array

---
+++ Array/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+      
+            int[] array = new int[9];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i + 1;
+                if (i == 4)
+                {
+                    array[i] = array[i - 1] * 2;
+                }
+                Console.WriteLine(array[i]);
+
+            }
+        }
+    }
+}
