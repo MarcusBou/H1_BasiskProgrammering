72620f586bfa05b1ee22f7b1f510f3519e7baec6
Marcus Damgaard Bournonwille <marc8847@zbc.dk>
2020-08-05 23:16:15 +0200

    po

---
+++ Array øvelse 5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Array_øvelse_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
